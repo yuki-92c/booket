d85785bb4d18c5e63d2735242f1f65d8334e4e0b
yuki-92c <jdpmcs@gmail.com>
2024-09-20 14:40:25 -0700

    fix: change Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,16 @@ import ThemeSwitch from "@/components/ThemeSwitch";
 export default function Header() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Booket</h1>
-      <ThemeSwitch />
+      <div className="flex justify-between">
+        <Link href={"/dashboard"}>
+          <h1 className="text-3xl font-bold mb-4">Booket</h1>
+        </Link>
+        <ThemeSwitch />
+      </div>
+
       <div className="p-2 rounded-md border border-red-300 w-36 items-center justify-center flex">
         <Link href={"/newPost"}>Create a Post</Link>
       </div>
-      <Link href={"/dashboard"}>dashboard</Link>
     </div>
   );
 }
